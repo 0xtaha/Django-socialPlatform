23b93a9b1a97a3099b0e253b626c23d30f95f0b5
0xtaha <57445405+0xtaha@users.noreply.github.com>
2019-12-01 18:43:09 +0200

    Add files via upload

---
+++ social-network/blog/models.py
@@ -0,0 +1,28 @@
+from django.db import models
+from django.utils import timezone
+from django.contrib.auth.models import User
+from django.urls import reverse
+
+# Create your models here.
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=100)
+    content = models.TextField()
+    date = models.TimeField(default=timezone.now)
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('post-detail', kwargs={'pk': self.pk})
+
+
+class Comment(models.Model):
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
+    comm = models.TextField()
+    date = models.TimeField(default=timezone.now)
+
+    def __str__(self):
+        return super().__str__()
